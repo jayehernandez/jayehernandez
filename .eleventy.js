1c721babc400a5ab6ba043d531128a1862417cd8
Jaye Hernandez <jaye@dealgrocer.com>
2020-06-22 21:12:27 +0800

    Add lazy loading for images

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPlugin(lazyImagesPlugin);
 
   return {
     dir: { input: 'src', output: '_site' }
