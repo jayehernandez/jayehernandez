c84ee95bea77b76d1c4362d3b7ec6de8b5a455ff
Jaye Hernandez <jaye@dealgrocer.com>
2020-06-22 21:18:15 +0800

    Lazy load images

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,15 @@ const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPlugin(lazyImagesPlugin);
+  eleventyConfig.addPlugin(lazyImagesPlugin, {
+    transformImgPath: (imgPath) => {
+      if (imgPath.startsWith('/') && !imgPath.startsWith('//')) {
+        return `./src${imgPath}`;
+      }
+
+      return imgPath;
+    },
+  });
 
   return {
     dir: { input: 'src', output: '_site' }
