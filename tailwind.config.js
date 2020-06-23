aae793e39536cc0e72658bc10a74e7b44359c84f
Jaye Hernandez <jaye@dealgrocer.com>
2020-06-23 20:46:24 +0800

    Complete rehaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
+const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: [
@@ -13,29 +14,33 @@ module.exports = {
           ...defaultTheme.fontFamily.sans,
         ]
       },
-      fontSize: {
-        '7xl': '5rem',
-        '8xl': '6rem',
-      },
       borderRadius: {
         'xl': '1.5rem',
+        'xxl': '5rem',
       },
+      colors: {
+        'teal': '#809888',
+        gray: {
+          '100': '#f5f5f5',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121',
+        },
+      }
     },
   },
-  variants: {},
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
   plugins: [
     plugin(function({ addBase, config }) {
       addBase({
         'h1': {
-          fontSize: config('theme.fontSize.7xl'),
-          fontFamily: 'Hillray-Regular',
-        },
-        'h2': {
-          fontSize: config('theme.fontSize.6xl'),
-          fontFamily: 'Hillray-Regular',
-        },
-        'h3': {
-          fontSize: config('theme.fontSize.4xl'),
           fontFamily: 'Hillray-Regular',
         },
       })
