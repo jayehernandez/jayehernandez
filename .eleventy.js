2de5d91c72059c56cc17cd82fb492529c160061e
Jaye Hernandez <jaye@dealgrocer.com>
2020-06-28 15:13:53 +0800

    Support webp images

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,38 @@
-const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const pluginLocalRespimg = require('eleventy-plugin-local-respimg');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPlugin(lazyImagesPlugin, {
-    transformImgPath: (imgPath) => {
-      if (imgPath.startsWith('/') && !imgPath.startsWith('//')) {
-        return `./src${imgPath}`;
-      }
 
-      return imgPath;
+  eleventyConfig.addPlugin(pluginLocalRespimg, {
+    folders: {
+      source: 'src',
+      output: '_site',
+    },
+    images: {
+      resize: {
+        min: 250,
+        max: 1500,
+        step: 150,
+      },
+      gifToVideo: false,
+      sizes: '100vw',
+      lazy: true,
+      watch: {
+        src: 'assets/**/*',
+      },
+      pngquant: {
+        speed: 10,
+        quality: [0.3, 0.5],
+      },
+      mozjpeg: {
+        quality: 60,
+      },
+      svgo: {},
+      gifresize: {},
+      webp: {
+        quality: 50,
+      },
+      gifwebp: {},
     },
   });
 
